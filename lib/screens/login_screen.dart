6204bf4f39b65d550ebe40b24c8b709ba256f931
Brayam <brayamalexander04@gmail.com>
2025-09-09 09:41:54 -0600

    feat(rived):animation added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,15 +9,62 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // Variable para controlar si se oculta o no la contraseña
+  bool _isObscure = true;
+
   @override
   Widget build(BuildContext context) {
+    // para obtener el tamaño de la pantalla del disp.
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
+      // evita notch o cámaras frontales
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(child: RiveAnimation.asset('assets/animated_login_character.riv')),
-          ],
-        )),
+        // margen interior
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                  width: size.width,
+                  height: 200,
+                  child: RiveAnimation.asset('assets/animated_login_character.riv')),
+              const SizedBox(height: 10),
+              // campo de texto del email
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  prefixIcon: const Icon(Icons.mail),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                ),
+              ),
+              const SizedBox(height: 10),
+              // campo de texto de la contraseña
+              TextField(
+                obscureText: _isObscure,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isObscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
